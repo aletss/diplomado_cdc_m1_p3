253943e79e8a1dbc3742794ad161069dab4f5303
unknown <551sosamurguia@gmail.com>
2025-11-27 22:09:33 -0600

    add notebook with analysis

--- ctg_viz/plots/histograms.py
+++ ctg_viz/plots/histograms.py
@@ -7,8 +7,6 @@ import numpy as np
 import plotly.graph_objects as go
 from scipy import stats
 
-df = pd.read_csv('D:/m1_practica3/CTG.csv')
-
 # Default color palettes
 MATPLOTLIB_COLORS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', 
                      '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
